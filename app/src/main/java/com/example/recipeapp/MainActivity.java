aeeaa9b9615acb347c72332265abd1ad1b58fed3
wilius36 <viliusbarauskas@yahoo.com>
2019-07-04 14:04:52 +0300

    Sutvarkytas app

--- app/src/main/java/com/example/recipeapp/MainActivity.java
+++ app/src/main/java/com/example/recipeapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.recipeapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import com.example.recipeapp.Fragments.InactiveUserFragments.FirstFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            FirstFragment f1= new FirstFragment();
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.add(R.id.main_frame, f1);
+            fragmentTransaction.addToBackStack("FirstFragment");
+            fragmentTransaction.commit();
+
+        }
+
     }
+
 }
